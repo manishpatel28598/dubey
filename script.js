fcc00bb1570059d70346c79e39279abf48aac04d
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-03-03 00:29:17 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// function supermarket(prices, n, k) {
+    // write code here
+    // do not console.log the answer
+    // return sorted array
+  //     prices = Number.parseInt(prices);
+  //     prices.sort();
+  //     let sum = 0;
+  //     for(let i=2; i<=k+2; i++){
+  //         sum += prices[i]; 
+  //     }
+  // return sum;
+  //   }
+  
+  // var readline = require("readline").createInterface(process.stdin);
+  
+  // let inputArr = [];
+  // var lineNumber = -1;
+  // var A;
+  // var B;
+  // readline.on("line", readInputs);
+  
+  // function readInputs(line) {
+  //   inputArr.push(line);
+  //   lineNumber++;
+  //   if (lineNumber == 0) {
+  //     [A, B] = inputArr[0].split(" ").map((x) => parseInt(x));
+  //   }
+  
+  //   if (lineNumber === 1) {
+  //     logic("s");
+  //     readline.close();
+  //   }
+  // }
+  
+  // function logic(input) {
+  //   //inputArr.shift();
+  //   const n = A;
+  //   const k = B;
+  //   const prices=inputArr[1].split(" ").map(x=>parseInt(x));
+  //   console.log(supermarket(prices,n,k))
+    
+  // }
+  // let x = "3"
+  // let y = 4
+
+  // let z = x+y;
+  // console.log(z)
+  // let str = "hello world"
+  // let str1 = str.slice(-5, -1)
+  // console.log(str1)
+  
+  // const a = {}
+  // const b = {key: 'b'}
+  // const c = {key: 'c'}
+  // a[b] = 123
+  // a[c] = 456
+  // console.log(a[b])
